dffd6b74b99c215f666b996a07275301ab0c4e76
A M S Rejuan <rejuan.shuvro@gmail.com>
2019-10-19 15:06:06 +0600

    initial commit

---
+++ src/main/java/com/shortandprecise/speedymock/SpeedymockApplication.java
@@ -0,0 +1,13 @@
+package com.shortandprecise.speedymock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedymockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedymockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shortandprecise/speedymock/SpeedymockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shortandprecise.speedymock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedymockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
